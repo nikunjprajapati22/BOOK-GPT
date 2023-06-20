3a3d28931729bdd0d3594fe9ee6f984db5bea364
NIkunj Prajapati <nikunjprajapati22@gmail.com>
2023-06-20 14:17:49 +0530

    build prompt template and script template

--- App.py
+++ App.py
@@ -13,3 +13,14 @@ os.environ['OPENAI_API_KEY'] = Mykey
 #App framework
 st.title('🦜🔗 GPT ')
 prompt = st.text_input('plug in your Prompt here')
+
+#prompt templates
+title_template = PromptTemplate(
+    input_variables = ['topic'],
+    template='write me a youtube video title about {topic}'
+)
+
+script_template = PromptTemplate(
+    input_variables = ['title', 'wikipedia_research'],
+    template='write me a youtube video script based on this title TITLE:{title} while leveraging this wikipedia research: {wikipedia_research}'
+)
