8fb60621bfb965fd5200228e77d6d0940e69d103
NIkunj Prajapati <nikunjprajapati22@gmail.com>
2023-06-21 08:59:19 +0530

    build conversion buffer and LLM chain

--- App.py
+++ App.py
@@ -24,3 +24,14 @@ script_template = PromptTemplate(
     input_variables = ['title', 'wikipedia_research'],
     template='write me a youtube video script based on this title TITLE:{title} while leveraging this wikipedia research: {wikipedia_research}'
 )
+
+#memory
+title_memory = ConversationBufferMemory(input_key = 'topic', memory_key = 'chat_history')
+script_memory = ConversationBufferMemory(input_key = 'title', memory_key = 'chat_history')
+
+#llms
+llm = OpenAI(temperature = 0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template,verbose=True, output_key='title', memory = title_memory)
+script_chain = LLMChain(llm=llm, prompt=script_template,verbose=True, output_key='script',memory= script_memory)
+
+wiki = WikipediaAPIWrapper()
